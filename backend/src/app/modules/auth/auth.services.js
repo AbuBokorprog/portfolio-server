c517d4bd7024c29994a0eecf7d0b8b90d77ff9cf
AbuBokorprog <abubokor1066@gmail.com>
2024-06-26 15:22:24 +0600

    password compare

--- backend/src/app/modules/auth/auth.services.js
+++ backend/src/app/modules/auth/auth.services.js
@@ -1,7 +1,7 @@
 import httpStatus from "http-status";
 import AppError from "../../errors/appError.js";
 import User from "../users/users.model.js";
-
+import bcrypt from "bcrypt";
 const loginUser = async (payload) => {
   const { email, password } = payload;
 
@@ -11,7 +11,13 @@ const loginUser = async (payload) => {
     throw new AppError(httpStatus.NOT_FOUND, "User not found");
   }
 
-  return data;
+  const isMatch = await bcrypt.compare(password, data.password);
+
+  if (!isMatch) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Password mismatched");
+  }
+
+  return isMatch;
 };
 
 export const authServices = { loginUser };
